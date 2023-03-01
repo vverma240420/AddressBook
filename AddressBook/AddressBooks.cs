08f7371ef72d981013ca9166da429f65cd1437c1
Vivek Verma <vverma240420@gmail.com>
2023-03-01 15:14:47 +0530

    UC1- Create Contact

---
+++ AddressBook/AddressBooks.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class AddressBooks
+    {
+        List<Contact> addressBookList = new List<Contact>();
+
+        public void CreateContact()
+        {
+            Contact contacts = new Contact();
+
+            Console.WriteLine("Enter First Name : ");
+            contacts.firstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name : ");
+            contacts.lastName = Console.ReadLine();
+            Console.WriteLine("Enter Email : ");
+            contacts.email = Console.ReadLine();
+            Console.WriteLine("Enter Phone Number : ");
+            contacts.phoneNo = Console.ReadLine();
+            Console.WriteLine("Enter the Address : ");
+            contacts.address = Console.ReadLine();
+            Console.WriteLine("Enter State Name : ");
+            contacts.state = Console.ReadLine();
+            Console.WriteLine("Enter the City Name : ");
+            contacts.city = Console.ReadLine();
+            Console.WriteLine("Enter Zip Code : ");
+            contacts.zipCode = Console.ReadLine();
+            Console.WriteLine("Contact created");
+        }
+    }
+}
---
+++ AddressBook/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("...Welcome to Address Book...");
+            AddressBooks Book = new AddressBooks();
+            Book.CreateContact();
+            Console.ReadLine();
+        }
+    }
+}
