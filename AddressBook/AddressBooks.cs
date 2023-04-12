d5575a6c2be7024c107ad00992a72b672cc8ccc5
Vivek Verma <vverma240420@gmail.com>
2023-04-12 11:30:50 +0530

    Added Program to check for duplicate contact

--- AddressBook/AddressBooks.cs
+++ AddressBook/AddressBooks.cs
@@ -8,33 +8,66 @@ namespace AddressBook
 {
     public class AddressBooks
     {
-        List<Contact> list = new List<Contact>();
+        public List<Contact> list = new List<Contact>();
+        public Dictionary<string, List<Contact>> contactDictionary = new Dictionary<string, List<Contact>>();
         Contact contact;
         public void Details()
         {
-            Console.WriteLine("Enter the number of Contact want to add");
-            int n = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < n; i++)
+            Console.WriteLine("Enter the Number of AddressBook do you want to make");
+            int numAddressBook = Convert.ToInt32(Console.ReadLine());
+            string addressBookName;
+            int num = 0;
+            while (num < numAddressBook)
             {
-                contact = new Contact();
-                Console.WriteLine("Enter first name");
-                contact.firstName = Console.ReadLine();
-                Console.WriteLine("Enter last name");
-                contact.lastName = Console.ReadLine();
-                Console.WriteLine("Enter Address");
-                contact.address = Console.ReadLine();
-                Console.WriteLine("Enter City");
-                contact.city = Console.ReadLine(); ;
-                Console.WriteLine("Enter State");
-                contact.state = Console.ReadLine();
-                Console.WriteLine("Enter Zip code");
-                contact.zip = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Enter Phone number");
-                contact.phoneNumber = Convert.ToInt64(Console.ReadLine());
-                Console.WriteLine("Enter Email");
-                contact.email = Console.ReadLine();
-                list.Add(contact);
-                //Console.WriteLine(contact);
+                Console.WriteLine("Enter addressbook name to add contacts");
+                addressBookName = Console.ReadLine();
+                Console.WriteLine("Enter the number of Contact want to add in {0} AddressBook", addressBookName);
+                int n = Convert.ToInt32(Console.ReadLine());
+                list = new List<Contact>();
+                for (int i = 0; i < n; i++)
+                {
+                    contact = new Contact();
+                    Console.WriteLine("Enter first name");
+                    contact.firstName = Console.ReadLine();
+                    Console.WriteLine("Enter last name");
+                    contact.lastName = Console.ReadLine();
+                    Console.WriteLine("Enter Address");
+                    contact.address = Console.ReadLine();
+                    Console.WriteLine("Enter City");
+                    contact.city = Console.ReadLine(); ;
+                    Console.WriteLine("Enter State");
+                    contact.state = Console.ReadLine();
+                    Console.WriteLine("Enter Zip code");
+                    contact.zip = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Enter Phone number");
+                    contact.phoneNumber = Convert.ToInt64(Console.ReadLine());
+                    Console.WriteLine("Enter Email");
+                    contact.email = Console.ReadLine();
+                    list.Add(contact);
+                    //Console.WriteLine(contact);
+                }
+                contactDictionary.Add(addressBookName, list);
+                num++;
+            }
+        }
+
+        public void DisplayAdressBook()
+        {
+            foreach (KeyValuePair<string, List<Contact>> keyValue in contactDictionary)
+            {
+                Console.WriteLine("AddressBook Name: " + keyValue.Key);
+                Console.WriteLine("\nDisplaying the person details\n");
+                foreach (Contact contact in keyValue.Value)
+                {
+                    Console.WriteLine("First Name:" + contact.firstName);
+                    Console.WriteLine("Last Name:" + contact.lastName);
+                    Console.WriteLine("Adress:" + contact.address);
+                    Console.WriteLine("City:" + contact.city);
+                    Console.WriteLine("Zip Code:" + contact.zip);
+                    Console.WriteLine("Phone Number:" + contact.phoneNumber);
+                    Console.WriteLine("Email:" + contact.email);
+                    Console.WriteLine("\n");
+                }
             }
         }
         public void Display()
@@ -121,5 +154,29 @@ namespace AddressBook
                 }
             }
         }
+        public void CheckDuplicateEntryOfAContact()
+        {
+            Console.WriteLine("Enter first name to which you want to  check duplicate entry ");
+            string name = Console.ReadLine();
+            bool isFound = false;
+            foreach (KeyValuePair<string, List<Contact>> keyValue in contactDictionary)
+            {
+                //Console.WriteLine("AddressBook Name: " + keyValue.Key);
+                //Console.WriteLine("\nDisplaying the person details\n");
+                foreach (Contact contact in keyValue.Value)
+                {
+                     //bool check = list.Any(s => name.ToLower()== contact.firstName.ToLower());//present in IEnumerable and determines whether the sequence satisfies a condition or not
+                    if (list.Any(s => name.ToLower() == contact.firstName.ToLower()))
+                    {
+                        Console.WriteLine("person named {0} is aleady present in address book {1}", name, keyValue.Key);
+                        isFound = true;
+                        break;
+                    }
+                }
+            }
+            if (isFound == false)
+                Console.WriteLine("contact does not Exists");
+        }
+
     }
 }
--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -9,7 +9,8 @@
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("1. Create Contact \n2. Edit Contact \n3. Delete Contact \n4. Add Multiple Contact \n5. Exit");
+                Console.WriteLine("1. Create Contact \n2. Edit Contact \n3. Delete Contact \n4. Add Multiple Contact" +
+                    " \n5. Create Multiple Address Book \n6. Check for duplicate entry \n7. Exit ");
                 int select = Convert.ToInt32(Console.ReadLine());
                 switch (select)
                 {
@@ -39,6 +40,17 @@
                         flag = false;
                         break;
                     case 5:
+                        addressBook.Details();
+                        addressBook.DisplayAdressBook();
+                        flag = false;
+                        break;
+                    case 6:
+                        addressBook.Details();
+                        addressBook.DisplayAdressBook();
+                        addressBook.CheckDuplicateEntryOfAContact();
+                        flag = false;
+                        break;
+                    case 7:
                         flag = false;
                         Console.WriteLine("Exist");
                         break;
